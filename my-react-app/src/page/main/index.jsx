0449b22c0f3c14e4926d86a9320f1b5a1af873a3
DimitrusB <dimitrusboss@mail.ru>
2024-02-01 23:08:11 +0300

    fix Search

--- my-react-app/src/page/main/index.jsx
+++ my-react-app/src/page/main/index.jsx
@@ -5,16 +5,21 @@ export const MainPage = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [weatherData, setWeatherData] = useState();
   const [calendarDate, setCalendarDate] = useState();
+  const [city, setCity] = useState("");
   const [iconWeather, setIconWeather] = useState();
 
-  const handleSearch = (event) => {
-    setSearchQuery(event.target.value);
+  const handleSearch = (e) => {
+    if (e.key === "Enter" || e.type === "click") {
+      e.preventDefault();
+      setCity(searchQuery);
+    }
   };
+
   useEffect(() => {
-    let city = "Санкт-Петербург";
-    let apiKey = "9be8e0b312d81d3bf00a9c3f9c2ef4b9";
+    if (!city) return;
+    let apiKey = "2fa6ce00ccea959715cea1bcdcd73110";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=ru&units=metric`;
-    let url5 = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=5&appid=${apiKey}`;
+    let url5 = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=ru&units=metric&cnt=5`;
 
     fetch(url)
       .then((response) => response.json())
@@ -25,25 +30,26 @@ export const MainPage = () => {
         let iconCode = data.weather[0].icon;
         let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         setIconWeather(iconUrl);
-      })
+            })
       .catch((err) => console.error("Error: ", err));
-  }, []);
+  }, [city]);
 
   console.log(weatherData);
-
-  let unixTimestamp = calendarDate;
-  let date = new Date(unixTimestamp * 1000);
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let today =
-    (day < 10 ? "0" + day : day) +
-    "." +
-    (month < 10 ? "0" + month : month) +
-    "." +
-    year;
-  console.log(today);
-
+  console.log(city);
+  let date, day, month, year, today;
+  if (calendarDate) {
+    let unixTimestamp = calendarDate;
+    date = new Date(unixTimestamp * 1000);
+    day = date.getDate();
+    month = date.getMonth() + 1;
+    year = date.getFullYear();
+    today =
+      (day < 10 ? "0" + day : day) +
+      "." +
+      (month < 10 ? "0" + month : month) +
+      "." +
+      year;
+  }
   return (
     <S.Wrapper>
       <h1>Погода по городам</h1>
@@ -52,15 +58,18 @@ export const MainPage = () => {
           type="search"
           placeholder="Введите название"
           name="search"
-          onChange={handleSearch}
-          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={handleSearch}
         />
-        <S.Search__btn type="submit">Найти</S.Search__btn>
+        <S.Search__btn onClick={handleSearch}>Найти</S.Search__btn>
       </S.Search__form>
+
       <p>Прогноз погоды на {today}</p>
       {Array.isArray(weatherData) &&
+        weatherData[0] &&
         weatherData.map((weather, index) => (
           <>
+            <h1>{weather.name}</h1>
             <p>Ветер:</p>
             <p>Скорость: {weather.wind.speed} м/с</p>
             <p>
